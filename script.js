f36788e8fb567ad92874501c8e69f198447c120a
egichuhis <gicheddy@gmail.com>
2023-07-24 11:51:25 +0300

    Add Hamburger menu toggle

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 /* ---------------------hamburger start here! below----------------------------- */
 const hamburger = document.querySelector('.hamburger');
 const Menu = document.querySelector('.navtabs');
-
+const backMenu = document.querySelectorAll('#list');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   Menu.classList.toggle('active');
 });
 
 /* ---------------------hamburger code above----------------------------- */
+backMenu.forEach((element) => element.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  Menu.classList.remove('active');
+}));
